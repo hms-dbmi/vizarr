073d6af0eea8215c46674455f87a5dfff33482e8
Trevor Manz <trevor.j.manz@gmail.com>
2020-08-06 10:16:54 -0400

    Setup gh-pages deploy (#8)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  assetPrefix:  process.env.GITHUB_JOB === 'build-and-deploy' ? '/vizarr/' : '',
   webpack: config => {
     config.resolve.alias['@hms-dbmi/viv'] = path.resolve(
       __dirname, './node_modules/@hubmap/vitessce-image-viewer/dist/bundle.es.js'
