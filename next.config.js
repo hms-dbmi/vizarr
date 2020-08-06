46c3c8bede3a218b7014ddeb2d0b47ff33f057ae
Trevor Manz <trevor.j.manz@gmail.com>
2020-08-06 10:09:08 -0400

    check process env vars

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
-const debug = process.env.NODE_ENV !== 'production'
-
 module.exports = {
-  assetPrefix: !debug ? '/vizarr/' : '',
+  assetPrefix:  process.env.GITHUB_JOB === 'build-and-deploy' ? '/vizarr/' : '',
   webpack: config => {
     config.resolve.alias['@hms-dbmi/viv'] = path.resolve(
       __dirname, './node_modules/@hubmap/vitessce-image-viewer/dist/bundle.es.js'
