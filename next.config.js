916d06a2e6dcafd56e030d877471d83d62ac3dbd
Trevor Manz <trevor.j.manz@gmail.com>
2020-08-06 02:20:02 -0400

    try asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
+  assetPrefix: !debug ? '/vizarr/' : '',
   webpack: config => {
     config.resolve.alias['@hms-dbmi/viv'] = path.resolve(
       __dirname, './node_modules/@hubmap/vitessce-image-viewer/dist/bundle.es.js'
