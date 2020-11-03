78cacf86edeee6c2b1c7225d72ade205c0da1718
Trevor Manz <trevor.j.manz@gmail.com>
2020-11-03 15:43:44 -0500

    add gridlayer and restore Viewer

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -3,9 +3,9 @@ import DeckGL from 'deck.gl';
 import { OrthographicView } from '@deck.gl/core';
 import type { Layer } from '@deck.gl/core';
 
-import { viewerViewState, layersSelector, sourceInfoState } from '../state';
-import { range } from '../utils';
+import { viewerViewState, layersSelector } from '../state';
 import type { VivLayerProps, ZarrLoader } from 'viv';
+
 
 function WrappedViewStateDeck({ layers }: { layers: Layer<VivLayerProps>[] }): JSX.Element {
   const [viewState, setViewState] = useRecoilState(viewerViewState);
@@ -18,8 +18,8 @@ function WrappedViewStateDeck({ layers }: { layers: Layer<VivLayerProps>[] }): J
     const zoom = -loader.numLevels;
     const target = [width / 2, height / 2, 0];
     setViewState({ zoom, target });
-  }
-
+  };
+  
   const views = [new OrthographicView({ id: 'ortho', controller: true })];
 
   return (
@@ -28,66 +28,11 @@ function WrappedViewStateDeck({ layers }: { layers: Layer<VivLayerProps>[] }): J
 }
 
 function Viewer(): JSX.Element {
-
-  const sourceInfo = useRecoilValue(sourceInfoState);
-
-  // Click handler for plate layout - opens image in new window
-  const handleClick = (info: Object) => {
-    let layerId = (info as any).layer.id;
-    if (!layerId.includes('-plate-')){
-      return;
-    }
-    // Get the info we need from the layerId
-    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const layerPropsId = layerId.split('-plate-')[0];
-    const [row, col] = layerId.split('-plate-')[1].split('-').map((x: string) => parseInt(x));
-    let source = (info as any).sourceLayer?.props?.source;
-    const plateAcquisitions = sourceInfo[layerPropsId]?.plateAcquisitions;
-    if (source && !isNaN(row) && !isNaN(col) && plateAcquisitions) {
-      if (source.endsWith('/')){
-        source = source.slice(0, -1);
-      }
-      let imgSource = `${source}/${plateAcquisitions[0]}/${letters[row]}/${col + 1}/Field_1/`;
-      window.open(window.location.origin + '?source=' + imgSource);
-    }
-  }
-
   const layerConstructors = useRecoilValue(layersSelector);
-  const layers = layerConstructors.flatMap((l) => {
+  const layers = layerConstructors.map((l) => {
     // Something weird with Recoil Loadable here. Need to cast to any.
     const { Layer, layerProps, on } = l as any;
-    const layerInfo = sourceInfo[layerProps.id];
-    const rows = layerInfo?.rows;
-    const columns = layerInfo?.columns;
-    const loaders = layerInfo.loaders;
-    if (rows && columns && layerProps.loader && loaders) {
-      const [height, width] = layerProps.loader.base.shape.slice(-2);
-      const spacer = 5;
-      const top = -(rows * (height + spacer)) / 2;
-      const left = -(columns * (width + spacer)) / 2;
-      return range(rows).flatMap((row) => {
-        return range(columns).flatMap((col) => {
-          const y = top + (row * (height + spacer));
-          const x = left + (col * (width + spacer));
-          // NB: this ID is used by onClick to get row and col
-          const id = `${layerProps.id}-plate-${row}-${col}`;
-          const loader = layerInfo.loaders ? layerInfo.loaders[col + (row * columns)]: undefined;
-          if (!loader) {
-            return [];
-          }
-          let wellProps = {
-            ...layerProps,
-            id,
-            translate: [x, y],
-            loader: layerInfo.loaders ? layerInfo.loaders[col + (row * columns)] : null,
-            pickable: true,
-            onClick: handleClick
-          }
-          return [new Layer(wellProps)]
-        });
-      })
-    }
-    return !Layer || !on ? [] : [new Layer(layerProps)];
+    return !Layer || !on ? null : new Layer(layerProps);
   });
   return <WrappedViewStateDeck layers={layers} />;
 }
