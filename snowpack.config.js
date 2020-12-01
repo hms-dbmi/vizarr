c42c2ecf6163608b3d1856d094f40a64fbf69191
Trevor Manz <trevor.j.manz@gmail.com>
2020-12-01 08:01:05 -0500

    Comment of custom rollup plugin for snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,22 +4,6 @@ const pkg = require('./package.json');
 // pkg version avaiable in app via import.meta.env.SNOWPACK_PUBLIC_PACKAGE_VERSION
 process.env.SNOWPACK_PUBLIC_PACKAGE_VERSION = pkg.version;
 
-function resolveGeotiff() {
-  return {
-    name: 'resolve-empty-geotiff',
-    async load(id) {
-      if (!id.includes('geotiff.js')) return;
-      // vizarr doesn't use geotiff component of viv.
-      // This plugin just creates an empty shim for top-level imports in viv,
-      // during install by snowpack.
-      return `
-      export const fromBlob = '';
-      export const fromUrl = '';
-      `;
-    },
-  }
-}
-
 module.exports = {
   mount: {
     public: '/',
@@ -52,3 +36,25 @@ module.exports = {
     /* ... */
   },
 };
+
+
+/* 
+* Custom Rollup Plugin for installing geotiff.js
+*
+* vizarr doesn't use geotiff component of viv.
+* This plugin just creates an empty shim for 
+* top-level imports in viv during install by snowpack.
+*/
+
+function resolveGeotiff() {
+  return {
+    name: 'resolve-empty-geotiff',
+    async load(id) {
+      if (!id.includes('geotiff.js')) return;
+      return `
+      export const fromBlob = '';
+      export const fromUrl = '';
+      `;
+    },
+  }
+}
