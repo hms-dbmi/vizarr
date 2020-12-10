b51c9250bc6c5dfc1b80abd0e94716ef080bfec1
Trevor Manz <trevor.j.manz@gmail.com>
2020-12-10 10:17:35 -0500

    prettier

--- src/zarr.ts
+++ src/zarr.ts
@@ -1,11 +1,11 @@
 import { addCodec } from '@manzt/zarr-lite';
 
 // @ts-ignore
-addCodec('gzip', () => import('numcodecs/gzip').then(m => m.default));
+addCodec('gzip', () => import('numcodecs/gzip').then((m) => m.default));
 // @ts-ignore
-addCodec('zlib', () => import('numcodecs/zlib').then(m => m.default));
+addCodec('zlib', () => import('numcodecs/zlib').then((m) => m.default));
 // @ts-ignore
-addCodec('blosc', () => import('numcodecs/blosc').then(m => m.default));
+addCodec('blosc', () => import('numcodecs/blosc').then((m) => m.default));
 
 export { openArray, HTTPStore } from '@manzt/zarr-lite';
 export type { Store, ZarrArray } from '@manzt/zarr-lite';
