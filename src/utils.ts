eb725444564a319e67db2a0ab9bd5d1f5a418c99
Trevor Manz <trevor.j.manz@gmail.com>
2021-02-21 13:46:34 -0500

    format

--- src/utils.ts
+++ src/utils.ts
@@ -98,7 +98,7 @@ export function fitBounds(
   [width, height]: [width: number, height: number],
   [targetWidth, targetHeight]: [targetWidth: number, targetHeight: number],
   maxZoom: number,
-  padding: number,
+  padding: number
 ) {
   const scaleX = (targetWidth - padding * 2) / width;
   const scaleY = (targetHeight - padding * 2) / height;
