b0cd9991aec141b545d2ec11e36f7f66b485e3c7
Trevor Manz <trevor.j.manz@gmail.com>
2021-06-25 11:09:47 -0400

    Wrap remote stores with a cache

---
+++ src/lru-store.ts
@@ -0,0 +1,36 @@
+import type { AsyncStore } from 'zarr/types/storage/types';
+import QuickLRU from 'quick-lru';
+
+export class LRUCacheStore<S extends AsyncStore<ArrayBuffer>> {
+  private cache: QuickLRU<string, ArrayBuffer>;
+
+  constructor(public store: S, public maxSize: number = 100) {
+    this.cache = new QuickLRU({ maxSize });
+  }
+
+  async getItem(...args: Parameters<S['getItem']>) {
+    const [key, opts] = args;
+    if (this.cache.has(key)) {
+      return this.cache.get(key)!;
+    }
+    const value = await this.store.getItem(key, opts);
+    this.cache.set(key, value);
+    return value;
+  }
+
+  async containsItem(key: string) {
+    return this.cache.has(key) || this.store.containsItem(key);
+  }
+
+  async keys() {
+    return [];
+  }
+
+  deleteItem(key: string): never {
+    throw new Error('deleteItem not implemented');
+  }
+
+  setItem(key: string, value: ArrayBuffer): never {
+    throw new Error('setItem not implemented');
+  }
+}
