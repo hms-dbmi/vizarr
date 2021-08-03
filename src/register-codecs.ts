28eba79d32f375dc4f398d2ae696d73cf9080723
Trevor Manz <trevor.j.manz@gmail.com>
2021-08-03 11:47:57 -0400

    Add description to registration module

--- src/register-codecs.ts
+++ src/register-codecs.ts
@@ -6,6 +6,7 @@ type CodecModule = { default: CodecConstructor<Record<string, unknown>> };
 const cache: Map<string, Promise<CodecModule['default']>> = new Map();
 
 function add(name: string, load: () => Promise<CodecModule>): void {
+  // Cache import to avoid duplicate loads
   const loadAndCache = () => {
     if (!cache.has(name)) {
       const promise = load()
@@ -21,6 +22,7 @@ function add(name: string, load: () => Promise<CodecModule>): void {
   addCodec(name, loadAndCache);
 }
 
+// Add dynmaically imported codecs to Zarr.js registry.
 add('lz4', () => import('numcodecs/lz4'));
 add('gzip', () => import('numcodecs/gzip'));
 add('zlib', () => import('numcodecs/zlib'));
