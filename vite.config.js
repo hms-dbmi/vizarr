339722ef99cff6738dc2b4c8e854c0628233a0e1
Trevor Manz <trevor.j.manz@gmail.com>
2021-10-08 10:45:47 -0400

    migrate to vite-pwa-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,47 @@
+// @ts-check
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import { resolve } from 'path';
 
+const manifest = {
+  name: 'vizarr',
+  short_name: 'vizarr',
+  lang: 'en-US',
+  start_url: '.',
+  display: 'standalone',
+  theme_color: '#000000',
+  icons: [
+    {
+      src: 'logo-512.png',
+      sizes: '512x512',
+      type: 'image/png',
+    },
+    {
+      src: 'logo-256.png',
+      sizes: '256x256',
+      type: 'image/png',
+    },
+    {
+      src: 'logo-192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+  ],
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt'],
+      manifest,
+      workbox: {
+        globPatterns: ['**/*.{js,json,ico,png,html,txt}'],
+      },
+    }),
+  ],
   base: process.env.VIZARR_PREFIX || './',
   build: {
     outDir: 'out',
--- workbox-config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  globDirectory: 'out/',
-  globPatterns: ['**/*.{js,json,ico,png,html,txt}'],
-  swDest: 'out/sw.js'
-};
