309993b3bca798f8d9f00d53baeb1b8c13efb670
William Moore <w.moore@dundee.ac.uk>
2022-01-05 13:29:09 +0000

    Handle v0.4 axes names and types

--- src/ome.ts
+++ src/ome.ts
@@ -61,7 +61,8 @@ export async function loadWell(config: ImageLayerConfig, grp: ZarrGroup, wellAtt
   // Create loader for every Image.
   const promises = imgPaths.map((p) => grp.getItem(join(p, resolution)));
   const data = (await Promise.all(promises)) as ZarrArray[];
-  const axis_labels = getOmeAxisLabels(imgAttrs.multiscales);
+  const axes = getOmeAxes(imgAttrs.multiscales)
+  const axis_labels = getOmeAxisLabels(axes);
 
   const tileSize = guessTileSize(data[0]);
   const loaders = range(rows).flatMap((row) => {
@@ -163,7 +164,8 @@ export async function loadPlate(config: ImageLayerConfig, grp: ZarrGroup, plateA
     { concurrency: 10 }
   );
   const data = await Promise.all(promises);
-  const axis_labels = getOmeAxisLabels(imgAttrs.multiscales);
+  const axes = getOmeAxes(imgAttrs.multiscales)
+  const axis_labels = getOmeAxisLabels(axes);
   const tileSize = guessTileSize(data[0][1]);
   const loaders = data.map((d) => {
     const [row, col] = d[0].split('/');
@@ -226,8 +228,9 @@ export async function loadOmeroMultiscales(
 ): Promise<SourceData> {
   const { name, opacity = 1, colormap = '' } = config;
   const data = await loadMultiscales(grp, attrs.multiscales);
-  const axis_labels = getOmeAxisLabels(attrs.multiscales);
-  const meta = parseOmeroMeta(attrs.omero, axis_labels);
+  const axes = getOmeAxes(attrs.multiscales)
+  const axis_labels = getOmeAxisLabels(axes);
+  const meta = parseOmeroMeta(attrs.omero, axes);
   const tileSize = guessTileSize(data[0]);
 
   const loader = data.map((arr) => new ZarrPixelSource(arr, axis_labels, tileSize));
@@ -262,7 +265,7 @@ async function defaultMeta(loader: ZarrPixelSource<string[]>, axis_labels: strin
   };
 }
 
-function parseOmeroMeta({ rdefs, channels, name }: Ome.Omero, axis_labels: string[]) {
+function parseOmeroMeta({ rdefs, channels, name }: Ome.Omero, axes: Ome.Axis[]) {
   const t = rdefs.defaultT ?? 0;
   const z = rdefs.defaultZ ?? 0;
 
@@ -278,11 +281,13 @@ function parseOmeroMeta({ rdefs, channels, name }: Ome.Omero, axis_labels: strin
     names.push(c.label);
   });
 
-  const defaultSelection = axis_labels.map((label) => {
-    if (label == 't') return t;
-    if (label == 'z') return z;
+  const defaultSelection = axes.map((axis) => {
+    if (axis.type == 'time') return t;
+    if (axis.name == 'z') return z;
     return 0;
   });
+  console.log("AXES", axes);
+  const channel_axis = axes.findIndex(axis => axis.type === "channel");
 
   return {
     name,
@@ -290,12 +295,38 @@ function parseOmeroMeta({ rdefs, channels, name }: Ome.Omero, axis_labels: strin
     colors,
     contrast_limits,
     visibilities,
-    channel_axis: axis_labels.includes('c') ? axis_labels.indexOf('c') : null,
+    channel_axis,
     defaultSelection,
   };
 }
 
-function getOmeAxisLabels(multiscales: Ome.Multiscale[]): [...string[], 'y', 'x'] {
-  const default_axes = ['t', 'c', 'z', 'y', 'x']; // v0.1 & v0.2
-  return (multiscales[0].axes || default_axes) as [...string[], 'y', 'x'];
+function getOmeAxes(multiscales: Ome.Multiscale[]): Ome.Axis[] {
+  // Returns axes in the latest v0.4+ format.
+  // defaults for v0.1 & v0.2
+  const default_axes = [
+    {type: 'time', name: 't'},
+    {type: 'channel', name: 'c'},
+    {type: 'space', name: 'z'},
+    {type: 'space', name: 'y'},
+    {type: 'space', name:'x'}
+  ];
+  let axes = default_axes;
+  // v0.3
+  const typesByName = { 'x': 'space', 'y': 'space', 'z': 'space', 'c': 'channel', 't': 'time'}
+  if (multiscales[0].axes) {
+    axes = multiscales[0].axes.map(axis => {
+      let name: string = axis instanceof String ? axis as string : (axis as Ome.Axis).name;
+      let type = typesByName[name] as string;
+      if (!(axis instanceof String) && (axis as Ome.Axis).type) {
+        type = (axis as Ome.Axis).type as string;
+      }
+      return {name, type}
+    });
+  }
+  return axes;
+}
+
+function getOmeAxisLabels(axes: Ome.Axis[]): [...string[], 'y', 'x'] {
+  const axes_names = axes.map(axis => axis.name);
+  return axes_names as [...string[], 'y', 'x'];
 }
