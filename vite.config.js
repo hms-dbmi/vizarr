f56d6287bc50e872de2cd1a0267ec1c8c094a76f
Trevor Manz <trevor.j.manz@gmail.com>
2024-07-25 00:03:50 -0400

    feat: Migrate to tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import * as path from "node:path";
 
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -7,26 +8,35 @@ import { defineConfig } from "vite";
  * Writes a new entry point that exports contents of an existing chunk.
  * @param {string} entryPointName - Name of the new entry point
  * @param {RegExp} chunkName - Name of the existing chunk
+ * @return {import("vite").Plugin}
  */
 function writeEntryPoint(entryPointName, chunkName) {
+  const jsFile = `${entryPointName}.js`;
+  const cssFile = `${entryPointName}.css`;
   return {
     name: "write-entry-point",
     async generateBundle(_, bundle) {
-      const chunk = Object.keys(bundle).find((key) => key.match(chunkName));
-      if (!chunk) {
+      const chunk = Object.values(bundle).find((key) => key.type === "chunk" && key.fileName.match(chunkName));
+      const styles = Object.values(bundle).find((key) => key.type === "asset" && key.fileName.match(chunkName));
+      if (!chunk || !styles) {
         throw new Error(`Could not find chunk matching ${chunkName}`);
       }
-      bundle[entryPointName] = {
-        fileName: entryPointName,
+      bundle[jsFile] = {
+        fileName: jsFile,
         type: "chunk",
-        code: `export * from './${chunk}';`,
+        code: `export * from './${chunk.fileName}';`,
+      };
+      bundle[cssFile] = {
+        fileName: cssFile,
+        type: "asset",
+        source: styles.source,
       };
     },
   };
 }
 
 export default defineConfig({
-  plugins: [react(), writeEntryPoint("index.js", /^vizarr-/)],
+  plugins: [react(), tailwindcss(), writeEntryPoint("index", /^vizarr-/)],
   base: process.env.VIZARR_PREFIX || "./",
   publicDir: path.resolve(__dirname, "assets"),
   build: {
@@ -36,9 +46,14 @@ export default defineConfig({
       output: {
         minifyInternalExports: false,
         manualChunks: {
-          vizarr: [path.resolve(__dirname, "src/index.tsx")],
+          vizarr: [path.resolve(__dirname, "./src/index.tsx")],
         },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
