bc782af5136c5cf7dfa043d1e1a3fcdad439a46a
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-27 22:36:57 -0500

    Add LUT support

--- src/layers/label-layer.ts
+++ src/layers/label-layer.ts
@@ -1,10 +1,10 @@
 import { getImageSize } from "@hms-dbmi/viv";
 import { BitmapLayer, TileLayer, type UpdateParameters } from "deck.gl";
 import * as utils from "../utils";
-import fs from "./label-layer-fragment";
 
 import { type Matrix4, clamp } from "math.gl";
 import type { ZarrPixelSource } from "../ZarrPixelSource";
+
 type Texture = ReturnType<BitmapLayer["context"]["device"]["createTexture"]>;
 
 export type LabelLayerLut = Readonly<Record<number, readonly [number, number, number, number]>>;
@@ -62,6 +62,7 @@ export class LabelLayer extends TileLayer<LabelPixelData> {
           pixelData: data,
           opacity: props.opacity,
           modelMatrix: props.modelMatrix,
+          lut: createLUT(props.lut),
           bounds: [clamp(left, 0, width), clamp(top, 0, height), clamp(right, 0, width), clamp(bottom, 0, height)],
           // For underlying class
           image: new ImageData(data.width, data.height),
@@ -72,7 +73,7 @@ export class LabelLayer extends TileLayer<LabelPixelData> {
   }
 }
 
-export class GrayscaleBitmapLayer extends BitmapLayer<{ pixelData: LabelPixelData }> {
+export class GrayscaleBitmapLayer extends BitmapLayer<{ pixelData: LabelPixelData; lut?: Float32Array }> {
   static layerName = "GrayscaleBitmapLayer";
   // @ts-expect-error - only way to extend the base state type
   state!: { texture: Texture } & BitmapLayer["state"];
@@ -80,12 +81,42 @@ export class GrayscaleBitmapLayer extends BitmapLayer<{ pixelData: LabelPixelDat
   getShaders() {
     const shaders = super.getShaders();
     // replace the builtin fragment shader with our own
-    return { ...shaders, fs };
+    return {
+      ...shaders,
+      fs: `\
+#version 300 es
+#define SHADER_NAME grayscale-bitmap-layer-fragment-shader
+
+precision highp float;
+
+uniform sampler2D grayscaleTexture;
+uniform float opacity;
+uniform vec3 lut[256];
+uniform bool useLUT;
+
+in vec2 vTexCoord;
+out vec4 fragColor;
+
+void main() {
+  float intensity = texture(grayscaleTexture, vTexCoord).r;
+  vec3 color;
+  if (useLUT) {
+    int index = int(floor(intensity * 255.0));
+    index = clamp(index, 0, 255);
+    color = lut[index];
+  } else {
+    color = vec3(1.0);
+  }
+  intensity = (intensity > 0.0) ? 1.0 : 0.0;
+  fragColor = vec4(color, intensity * opacity);
+}
+`,
+    };
   }
 
   updateState({ props, oldProps, changeFlags, ...rest }: UpdateParameters<this>): void {
     super.updateState({ props, oldProps, changeFlags, ...rest });
-    if (false && props.pixelData !== oldProps.pixelData || changeFlags.dataChanged) {
+    if (props.pixelData !== oldProps.pixelData || changeFlags.dataChanged) {
       if (this.state.texture) {
         this.state.texture.destroy();
       }
@@ -110,6 +141,13 @@ export class GrayscaleBitmapLayer extends BitmapLayer<{ pixelData: LabelPixelDat
 
   draw(opts: unknown) {
     const { texture } = this.state;
+    if (this.props.lut) {
+    }
+    this.state.model?.setUniforms({
+      // @ts-expect-error - Float32Array is ok here
+      lut: this.props.lut ?? new Float32Array(),
+      useLUT: !!this.props.lut,
+    });
     if (texture) {
       this.state.model?.setBindings({ grayscaleTexture: texture });
     }
@@ -124,4 +162,58 @@ function getTileSizeForResolutions(resolutions: Array<ZarrPixelSource>): number 
     "resolutions must all have the same tile size",
   );
   return tileSize;
+}
+
+function createLUT(source?: LabelLayerLut) {
+  if (source) {
+    const lut = new Float32Array(256 * 3);
+    for (let [value, color] of Object.entries(source)) {
+      lut[+value * 3 + 0] = color[0];
+      lut[+value * 3 + 1] = color[1];
+      lut[+value * 3 + 2] = color[2];
+    }
+    return lut;
+  }
+  return generateCategoricalLUT();
+}
+
+const palettes = {
+  pastel1: [
+    [251, 180, 174],
+    [179, 205, 227],
+    [204, 235, 197],
+    [222, 203, 228],
+    [254, 217, 166],
+    [255, 255, 204],
+    [229, 216, 189],
+    [253, 218, 236],
+    [242, 242, 242],
+  ],
+  set3: [
+    [141, 211, 199],
+    [255, 255, 179],
+    [190, 186, 218],
+    [251, 128, 114],
+    [128, 177, 211],
+    [253, 180, 98],
+    [179, 222, 105],
+    [252, 205, 229],
+    [217, 217, 217],
+    [188, 128, 189],
+  ],
+} as const;
+
+function generateCategoricalLUT(
+  palette: ReadonlyArray<readonly [number, number, number, a?: number]> = palettes.pastel1,
+) {
+  const lut = new Float32Array(256 * 3);
+
+  for (let i = 0; i < 256; i++) {
+    const color = palette[i % palette.length];
+    lut[i * 3 + 0] = color[0] / 255.0;
+    lut[i * 3 + 1] = color[1] / 255.0;
+    lut[i * 3 + 2] = color[2] / 255.0;
+  }
+
+  return lut;
 }
