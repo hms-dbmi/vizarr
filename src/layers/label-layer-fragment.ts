21cbd8afe6fccf9099393a8c445f88f8b06a64d6
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-27 21:54:53 -0500

    Min-max the shader

--- src/layers/label-layer-fragment.ts
+++ src/layers/label-layer-fragment.ts
@@ -4,14 +4,15 @@ export default `\
 
 precision highp float;
 
-uniform sampler2D uGrayscaleTexture;
+uniform sampler2D grayscaleTexture;
 uniform float opacity;
 
 in vec2 vTexCoord;
 out vec4 fragColor;
 
 void main() {
-    float intensity = texture(uGrayscaleTexture, vTexCoord).r;
-    fragColor = vec4(vec3(intensity), intensity * opacity);
+  float intensity = texture(grayscaleTexture, vTexCoord).r;
+  intensity = (intensity > 0.0) ? 1.0 : 0.0;
+  fragColor = vec4(vec3(intensity), intensity * opacity);
 }
 `;
